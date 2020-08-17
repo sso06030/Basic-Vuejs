53848b8acbe7a9999e83492c0c90ba424dfcad23
kim-yeji <sso06030@khu.ac.kr>
2020-08-17 16:19:26 +0900

    test1 project

--- test1/src/views/Home.vue
+++ test1/src/views/Home.vue
@@ -1,35 +1,18 @@
-<!-- html 코드 --> 
 <template>
-  <!-- 가장 바깥에 태그로 전체를 감싸야함 --> 
-  <div>
-    <h1>This is Home page</h1>
-    <Kozy title="home title" name="kozy"/>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<!-- 옵션 : data, method, computed, watch... -->
 <script>
-//@ : src를 가리킨다
-import Kozy from '@/components/Kozy.vue';
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
-export default{
-  components:{
-      Kozy
-  },
-
-  //data는 함수로 정의 후 obj를 리턴해야함
-  data(){
-    return {
-      //name:'Kozy'
-    }
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
   }
 }
 </script>
-
-<!-- css 코드 -->
-<!-- scoped : 전역이 아닌 해당 컴포넌트 내에서만 적용 -->
-<style scoped>
-h1 {
-  color:red;
-}
-</style>
