cbe96269eb12cde8115ee3baf9c5cd6566fa455d
kim-yeji <sso06030@khu.ac.kr>
2020-08-17 17:32:49 +0900

    instance life cycle

--- test1/src/views/Home.vue
+++ test1/src/views/Home.vue
@@ -7,7 +7,7 @@
       <InputField :name="name" @update-name="name=$event.target.value" />-->
       <InputField v-model="name" />
       <br />
-      <button>Submit</button>
+      <button @click="updateName">Submit</button>
     </form>
     {{ name }}
   </div>
@@ -22,14 +22,48 @@ export default {
   },
   data() {
     return {
-      name: "",
+      name: "kozy",
     };
   },
-  // methods: {
-  //   updateName(name) {
-  //     this.name = name;
-  //   },
-  // },
+  beforeCreate() {
+    console.log("beforeCreate", this.name);
+  },
+  //데이터 접근 가능
+  //서버에 요청을 보내서 응답을 받아온 데이터를 업데이트 해줄 때
+  created() {
+    console.log("created", this.name);
+  },
+
+  beforeMount() {
+    alert("beforeMount");
+  },
+  //화면이 dom에 접근 가능
+  //dom을 control 하고 싶은 경우
+  //화면에 정보가 보여짐
+  mounted() {
+    alert("mounted");
+  },
+  beforeUpdate() {
+    alert("beforeUpdate");
+  },
+  updated() {
+    alert("updated");
+  },
+  //about 페이지로 넘어가면 home 컴포넌트가 없어짐
+  beforeDestroy() {
+    alert("beforeDestroy");
+  },
+  //나갈 때 메모리 누수를 방지하기 위해서
+  //eventlistener를 삭제해주거나 데이터를 초기화해주는 작업을 함
+  destroyed() {
+    alert("destroyed");
+  },
+
+  methods: {
+    updateName() {
+      this.name = "updated";
+    },
+  },
 };
 </script>
 
